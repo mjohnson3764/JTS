cc151d91de046231a3f18c96a180b656f64ec197
mjohnson3764 <34069149+mjohnson3764@users.noreply.github.com>
2018-01-23 20:13:38 -0500

    Update PHP

--- form-to-email.php
+++ form-to-email.php
@@ -1,5 +1,6 @@
 <?php
 $action=$_REQUEST['action'];
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 if ($action=="")    /* display the contact form */
     {
     ?>
@@ -11,7 +12,7 @@ if ($action=="")    /* display the contact form */
 			<input type="submit" value="Submit">
 		</form>
     <?php
-    } 
+    }
 else                /* send the submitted data */
     {
     $name=$_REQUEST['name'];
@@ -19,13 +20,13 @@ else                /* send the submitted data */
     $message=$_REQUEST['message'];
     if (($name=="")||($email=="")||($message==""))
         {
-		echo "All fields are required, please fill <a href="contact.html">the form</a> again.";
+		echo "All fields are required, please fill <a href=\"contact.html\">the form</a> again.";
 	    }
-    else{		
+    else{
 	    $from="From: $name<$email>\r\nReturn-path: $email";
         $subject="Message sent from JohnsonTurbine.com";
-		// mail("sales@johnsonturbine.com", $subject, $message, $from);
+		 mail("sales@johnsonturbine.com", $subject, $message, $from);
 		echo "Email sent!";
 	    }
-    }  
+    }
 ?>
